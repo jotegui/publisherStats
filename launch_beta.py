f8c77ccc16b44658fb04cb01fe735e4f5081489c
Javier Otegui <javier.otegui@gmail.com>
2014-03-13 16:32:20 -0600

    After 2nd Beta

--- launch_beta.py
+++ launch_beta.py
@@ -12,14 +12,14 @@ logging.info('Initiated at {0}'.format(ini))
 
 stat_reports_path = './statReports2014_03_04.json'
 
-try:
-    logging.info('Trying to delete previous reports. Info taken from file {0}'.format(stat_reports_path))
-    db.main(stat_reports_path = stat_reports_path)
-    logging.info('Deletion finished. Check log for result')
-except:
-    logging.warning('File not found, avoiding deletion.')
+#try:
+#    logging.info('Trying to delete previous reports. Info taken from file {0}'.format(stat_reports_path))
+#    db.main(stat_reports_path = stat_reports_path)
+#    logging.info('Deletion finished. Check log for result')
+#except:
+#    logging.warning('File not found, avoiding deletion.')
 
-monthlyStatReports.main(lapse='month', testing=True)
+monthlyStatReports.main(lapse='month', testing=False, beta=True)
 
 end = datetime.now()
 dif = end - ini

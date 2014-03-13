92018b84e835a4ab676c1b4eec655f68b33d16ef
Javier Otegui <javier.otegui@gmail.com>
2014-03-13 15:31:08 -0600

    NOW ready for second round of beta-testing

--- deleteBetaReports.py
+++ deleteBetaReports.py
@@ -4,7 +4,7 @@ import uploadToGithub as up
 
 
 def main(stat_reports_path):
-    key = apikey(testing=True)
+    key = apikey(testing=False)
     git_urls = json.loads(open(stat_reports_path, 'r').read().rstrip())
     up.deleteAll(git_urls, key)
 

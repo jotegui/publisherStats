1c360eff52fcf1f81072010b40c7f1978a765b51
Javier Otegui <javier.otegui@gmail.com>
2014-03-25 16:41:05 -0600

    Updated extraction of legacy data

--- legacyData.py
+++ legacyData.py
@@ -2,10 +2,11 @@ import json
 import urllib2
 from urllib import urlencode
 from datetime import datetime
-from extractStats import fieldList, getObject, getCountsForPublishers
+from extractStats import get_gcs_counts
 
 base_url = 'https://www.googleapis.com/storage/v1beta2'
 downloads_bucket = 'vn-downloads'
+
 
 # Extract list of files in bucket
 def getObjectList(bucket_name):
@@ -16,6 +17,7 @@ def getObjectList(bucket_name):
     for i in raw['items']:
         l.append([i['name'], datetime.strptime(i['updated'].split('T')[0], '%Y-%m-%d')])
     return l
+
 
 def filterByDate(object_list, year, month = None):
     new_list = []
@@ -28,30 +30,31 @@ def filterByDate(object_list, year, month = None):
             new_list.append("/gs/vn-downloads/{0}".format(o[0]))
     return new_list
 
+
 def main(year, month = None):
     object_list = getObjectList(downloads_bucket)
     filtered_list = filterByDate(object_list, year, month)
     
-    pubs = getCountsForPublishers(filtered_list)
+    pubs = get_gcs_counts(filtered_list)
     
     if month is None:
         lapse = "year"
     else:
         lapse = "month"
-    
+    docs = {}
     for pub in pubs:
         doc = {lapse:{}}
         doc[lapse]['downloads'] = len(pubs[pub]['download_files'])
         doc[lapse]['downloads_period'] = pubs[pub]['downloads_in_period']
         doc[lapse]['records'] = pubs[pub]['records_downloaded']
         doc[lapse]['records_period'] = pubs[pub]['tot_recs']
-        
-        json.dump(doc, open('./reports_2013/{0}.json'.format(pub),'w'))
+        docs[pub] = doc
+        # json.dump(doc, open('./reports_2013/{0}.json'.format(pub),'w'))
     
-    return
+    return docs
 
 if __name__ == "__main__":
     
     year = 2013
-    month = None
-    main(year, month)
+    month = 12
+    doc = main(year, month)
